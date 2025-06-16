83ca6b26f27a71dd0fb0fe391f4d38bb0557c686
meisammofidi <meisam.mofidi@yahoo.com>
2025-06-16 01:48:22 -0400

    Remove test routes

--- app/fetchtest/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function FetchTest() {
-  const result = await fetch("http://localhost:3000/api/hello", {
-    method: "GET",
-  });
-  const data = await result.json();
-
-  return <h1>{data.message}</h1>;
-}
